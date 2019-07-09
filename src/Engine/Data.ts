6cae3e69cc0303f00ded651805a0466ec70cad2d
1MrSmith <48734090+1MrSmith@users.noreply.github.com>
2019-07-09 16:46:10 +0300

    fix: changed type request(https)

--- src/Engine/Data.ts
+++ src/Engine/Data.ts
@@ -1,9 +1,9 @@
 
 export function loadDataByID(id: number): string {
     const date: Date = new Date();
-    return 'http://www.nbrb.by/API/ExRates/Rates/Dynamics/' + id + '?startDate=' + [date.getFullYear(), date.getMonth(), date.getDay()].join('-') + '&endDate=' + [date.getFullYear(), date.getMonth() + 1, date.getDay()].join('-');
+    return 'https://www.nbrb.by/API/ExRates/Rates/Dynamics/' + id + '?startDate=' + [date.getFullYear(), date.getMonth(), date.getDay()].join('-') + '&endDate=' + [date.getFullYear(), date.getMonth() + 1, date.getDay()].join('-');
 }
 
 export function loadData(): string {
-    return 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0';
+    return 'https://www.nbrb.by/API/ExRates/Rates?Periodicity=0';
 }
