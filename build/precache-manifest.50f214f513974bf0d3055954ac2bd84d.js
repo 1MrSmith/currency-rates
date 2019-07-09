9228f508dcc4254b1fabd6701ec9b141b512f3e3
1MrSmith <48734090+1MrSmith@users.noreply.github.com>
2019-07-09 16:27:17 +0300

    feat: deploy site

---
+++ build/precache-manifest.50f214f513974bf0d3055954ac2bd84d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54482800e96f2c12c3ef75aedb09c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a5769a4078997eed56",
+    "url": "/static/css/main.bc73010d.chunk.css"
+  },
+  {
+    "revision": "c67e8f8f00605f2e2dbd",
+    "url": "/static/js/2.be751068.chunk.js"
+  },
+  {
+    "revision": "93a5769a4078997eed56",
+    "url": "/static/js/main.c27e99fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
